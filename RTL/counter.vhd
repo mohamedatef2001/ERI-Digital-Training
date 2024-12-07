0c5dc169be50a135a9151696a9b4e04214ea9743
mohamedatef2001 <168041996+mohamedatef2001@users.noreply.github.com>
2024-12-07 14:30:06 +0200

    Edit file
    edit counter.vhdl file

--- RTL/counter.vhd
+++ RTL/counter.vhd
@@ -52,22 +52,21 @@ architecture Behavioral of counter is
 signal s_counter : std_logic_vector (3 downto 0) ;
 
 begin
-process (i_clk , i_reset)
-begin
-if i_reset = '0' then
-s_counter <= (others => '0') ;
-o_q0 <= '0' ;
-o_q1 <= '0' ;
-o_q2 <= '0' ;
-o_q3 <= '0' ;
-elsif i_clk = '1' then
-if i_load = '1' then
-s_counter <= (i_3 & i_2 &i_1 &  i_0 );
-else
-s_counter <= s_counter +1 ;
-(o_q3,o_q2,o_q1,o_q0) <= s_counter ;
-end if;
-end if ;
-end process ;
-
+   process (i_clk , i_reset)
+    begin
+	 if i_reset = '0' then
+	     s_counter <= (others => '0') ;
+         o_q0 <= '0' ;
+         o_q1 <= '0' ;
+         o_q2 <= '0' ; 
+         o_q3 <= '0' ;
+	 elsif i_clk = '1' then
+	     if i_load = '1' then
+			 s_counter <= (i_3 & i_2 &i_1 &  i_0 );
+		 else
+			 s_counter <= s_counter +1 ;
+             (o_q3,o_q2,o_q1,o_q0) <= s_counter ;
+		end if;
+	 end if ;
+	end process ;
 end Behavioral;
