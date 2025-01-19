4f884b575366e932e670c8f8217311ca7ecc13df
mohamedatef2001 <168041996+mohamedatef2001@users.noreply.github.com>
2025-01-19 09:03:17 +0200

    Add file

--- AXI4-Stream_FIR_Filter/SDK/src/HD_SW_FIR_FILTER.c
+++ AXI4-Stream_FIR_Filter/SDK/src/HD_SW_FIR_FILTER.c
@@ -1,13 +1,17 @@
 #include <stdlib.h>
 #include <xuartps.h>
 #include <xparameters.h>
+#include <time.h>
+#include <sleep.h>
 #include <xaxidma.h>
 #include <xil_cache.h>
+#include <xil_printf.h>
 //If you create a file without specifying a type and fill it with zeros and ones, the file won't have a header.
 //Headers are typically added by specific file formats (like .jpg, .png, .mp3, etc.) to store metadata about the file. Since your file is just raw binary data,
 //it won't include any header information.
 //The size of the file will be determined solely by the amount of data you put into it.
-#define fileSize 1800
+
+#define fileSize 400
 
 XAxiDma_Config  *MyDmaConfig ;
 XAxiDma MyDMA;
@@ -28,24 +32,24 @@ u32 checkHalted(u32 baseAddress,u32 offset);
 int main(){
 
 //Memory allocation Step
-u32 *fileData ;
+u8 *fileData ;
 fileData = malloc (sizeof(u8)*(fileSize));
 if (fileData <= 0){
 	print("Memory Allocation Failed... \n");
 	return -1;
     }
-print("Memory Allocation success..\n");
+//print("Memory Allocation success..\n");
 
 //-------------------------------DMA initialization----------------------------------//
  //Look up the hardware configuration for a device instance based on base address
- MyDmaConfig = XAxiDma_LookupConfigBaseAddr(XPAR_AXI_DMA_0_BASEADDR);
+ MyDmaConfig = XAxiDma_LookupConfigBaseAddr(XPAR_FILTER_FIR_DMA_BASEADDR);
  //This function initializes a DMA engine.
  status = XAxiDma_CfgInitialize(&MyDMA, MyDmaConfig);
  if(status!=XST_SUCCESS){
 	 print("DMA initialization failed");
 	 return -1;
  }
- print("DMA initialization success..\n");
+// print("DMA initialization success..\n");
 
 //-------------------------------UART initialization------------------------------------//
  // Looks up the device configuration based on the unique device ID. The table
@@ -53,22 +57,32 @@ print("Memory Allocation success..\n");
 
  // @return	A pointer to the configuration structure or NULL if the
  // specified device is not in the system.
- MyUartConfig = XUartPs_LookupConfig(XPAR_PS7_UART_1_DEVICE_ID);
+ MyUartConfig = XUartPs_LookupConfig(XPAR_PS7_UART_0_DEVICE_ID);
  status = XUartPs_CfgInitialize(&MyUart,MyUartConfig,MyUartConfig->BaseAddress );
  if(status!=XST_SUCCESS){
 	 print("UART initialization failed");
 	 return -1;
  }
- print("UART initialization success..\n");
+ //print("UART initialization success..\n");
+
+ //-------------------------------------Baud Rate----------------------------------//
+
+ status = XUartPs_SetBaudRate(&MyUart, 115200);
+ if(status!=XST_SUCCESS){
+	 print("SetBaudRate failed");
+	 return -1;
+ }
 
  //-----------------------------Receive from PC to Memory--------------------------//
  //This function attempts to receive a specified number of bytes of data
  // from the device and store it into the specified buffer.
  //* @return	The number of bytes received.
  while(TotalReceivedBytes<fileSize){
- ReceivedBytes =  XUartPs_Recv(&MyUart,(u8*)&fileData[TotalReceivedBytes],10);
+ ReceivedBytes =  XUartPs_Recv(&MyUart,(u8*)&fileData[TotalReceivedBytes],100);
  TotalReceivedBytes += ReceivedBytes;
+
  }
+
 
  //----------------------------------Flush a Data cache --------------------------//
  // To force the processor to write 'fileData' into the memory immediately
@@ -78,18 +92,19 @@ print("Memory Allocation success..\n");
 
  // It's better to enable sending XAXIDMA_DEVICE_TO_DMA first to make sure the internal memory of the DMA
  // does not get full and halt.
+
  //-----------------------------XAXIDMA_DEVICE_TO_DMA--------------------------//
  /// Note that this function just configures the internal register of the DMA,
  /// where it needs the data and how much it needs to send.
  /// But this function doesn't wait for all the data to be sent.
- status = XAxiDma_SimpleTransfer(&MyDMA, (u32)*fileData,fileSize ,XAXIDMA_DEVICE_TO_DMA);
+ status = XAxiDma_SimpleTransfer(&MyDMA, (u32)&fileData, fileSize ,XAXIDMA_DEVICE_TO_DMA);
  if(status!=XST_SUCCESS){
 	 print("XAXIDMA_DEVICE_TO_DMA failed");
 	 return -1;
  }
 
  //-----------------------------XAXIDMA_DMA_TO_DEVICE--------------------------//
- status = XAxiDma_SimpleTransfer(&MyDMA, (u32)*fileData,fileSize ,XAXIDMA_DMA_TO_DEVICE);
+ status = XAxiDma_SimpleTransfer(&MyDMA, (u32)&fileData,fileSize ,XAXIDMA_DMA_TO_DEVICE);
  if(status!=XST_SUCCESS){
 	 print("XAXIDMA_DEVICE_TO_DMA failed");
 	 return -1;
@@ -99,24 +114,29 @@ print("Memory Allocation success..\n");
 //-------------------------------checkHalted-----------------------------------------//
 // Check if either DMA internal memory gets full during sending and receiving data. It will stay in that loop until
 // all data is transferred.
- status = checkHalted(XPAR_AXI_DMA_0_BASEADDR,0x4);
+ status = checkHalted(XPAR_FILTER_FIR_DMA_BASEADDR,0x4);
     while(status != 1){
-    	status = checkHalted(XPAR_AXI_DMA_0_BASEADDR,0x4);
+    	status = checkHalted(XPAR_FILTER_FIR_DMA_BASEADDR,0x4);
     }
-    status = checkHalted(XPAR_AXI_DMA_0_BASEADDR,0x34);
+    status = checkHalted(XPAR_FILTER_FIR_DMA_BASEADDR,0x34);
     while(status != 1){
-    	status = checkHalted(XPAR_AXI_DMA_0_BASEADDR,0x34);
+    	status = checkHalted(XPAR_FILTER_FIR_DMA_BASEADDR,0x34);
     }
-	print("DMA transfer success..\n");
+	//print("DMA transfer success..\n");
 
 //------------------------------Sending data to PC-----------------------------------//
-while(TotalReceivedBytes<fileSize){
-TransmittedBytes =  XUartPs_Recv(&MyUart,(u8*)&fileData[TotalTransmittedBytes],10);
- TotalReceivedBytes += TransmittedBytes;
+while(TotalTransmittedBytes<fileSize){
+//xil_printf("data = %0x", (u8*)&fileData[TotalTransmittedBytes]);
+TransmittedBytes =  XUartPs_Send(&MyUart,(u8*)&fileData[TotalTransmittedBytes],1);
+TotalTransmittedBytes += TransmittedBytes;
+ usleep(1000) ;
 }
 
 
+
+
 }
+
 
 u32 checkHalted(u32 baseAddress,u32 offset){
 		u32 status;
